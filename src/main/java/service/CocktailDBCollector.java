2b101367ecb348a4b543aa7023246d072c489e9a
nwill30 <techexpressnw@gmail.com>
2018-09-24 21:58:17 -0500

    build complete drink list

--- src/main/java/service/CocktailDBCollector.java
+++ src/main/java/service/CocktailDBCollector.java
@@ -17,21 +17,38 @@ public class CocktailDBCollector {
     private static final String baseURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?";
 
     public void getAndCacheCocktails() {
-        String drinkListJson = getDrinkList();
-        DrinkList drinkList = mapListObject(drinkListJson);
-//        setDrinkListItems(drinkList);
-        for(Drinks i : drinkList.getDrinks()){
-            System.out.println("Drink Name:" + i.getStrDrink() +"; ID:" + i.getIdDrink());;
+        DrinkList completeDrinkList = getCompleteDrinkList();
+
+        for(Drinks i : completeDrinkList.getDrinks()){
+            System.out.println("Drink: "+i.getStrDrink() + " ID: "+i.getIdDrink());
         }
+
     }
 
+    private DrinkList getCompleteDrinkList() {
+        String ordinaryDrinkListString = httpGetResponse(baseURL + "c=Ordinary_Drink");
+        String cocktailDrinkListString = httpGetResponse(baseURL + "c=Cocktail");
+        DrinkList ordinaryDrinkList = mapListObject(ordinaryDrinkListString);
+        DrinkList cocktailDrinkList = mapListObject(cocktailDrinkListString);
+        DrinkList completeList = new DrinkList();
+        completeList.setDrinks(ordinaryDrinkList.getDrinks());
+        completeList.getDrinks().addAll(cocktailDrinkList.getDrinks());
+        return completeList;
+    }
 
+    /**
+     * Method uses http get request to hit provided endpoint and store response in String Entity
+     * a rest template is provided request instructions via the request entity
+     * The request response is stored in a generics RequestEntity
+     * @param requestURI is the url/endpoint to be processed
+     *                   @return the endpoint response body is returned to the calling method
+     * */
+    private String httpGetResponse(String requestURI) {
 
-    private String getDrinkList() {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> stringResponseEntity = null;
         try {
-            RequestEntity<String> requestEntity = new RequestEntity<String>(HttpMethod.GET, new URI(baseURL + "c=Ordinary_Drink"));
+            RequestEntity<String> requestEntity = new RequestEntity<String>(HttpMethod.GET, new URI(requestURI));
             stringResponseEntity = restTemplate.exchange(requestEntity, String.class);
         }catch (URISyntaxException e) {
             e.printStackTrace();
@@ -50,12 +67,5 @@ public class CocktailDBCollector {
         return mappedDrinkList;
     }
 
-//    private void setDrinkListItems(DrinkList drinkList){
-//        ObjectMapper mapper = new ObjectMapper();
-//        try{
-//            drinkList.setDrinkIDList(mapper.readValue(drinkListJson,DrinkList.class));
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//    }
+
 }
